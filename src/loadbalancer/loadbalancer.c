e7bc3c4f1c01c6cdac7794393777b4f8edb06a47
Tim Stegeman <tim.stegeman@gmail.com>
2022-12-31 20:53:26 +0100

    Simplify lb

--- src/loadbalancer/loadbalancer.c
+++ src/loadbalancer/loadbalancer.c
@@ -13,7 +13,7 @@ static lb_limit_charger_cb_t lb_limit_charger_cb;
 static uint16_t grid_current[3];
 static int charger_max_current;
 static enum lb_state state;
-static uint8_t wait_time, grid_current_age;
+static uint8_t wait_time, fallback_time;
 static int charger_limit_override;
 
 void lb_init(struct lb_config* config_, lb_limit_charger_cb_t lb_limit_charger_cb_) {
@@ -22,7 +22,7 @@ void lb_init(struct lb_config* config_, lb_limit_charger_cb_t lb_limit_charger_c
   memset(grid_current, 0, sizeof(grid_current));
   state = LB_STATE_NORMAL;
   wait_time = WAIT_TIME_UNSET;
-  grid_current_age = WAIT_TIME_UNSET;
+  fallback_time = 0;
   charger_max_current = 0;  // We start at zero and gradually go up
 
   charger_limit_override = config_->charger_limit;
@@ -30,7 +30,7 @@ void lb_init(struct lb_config* config_, lb_limit_charger_cb_t lb_limit_charger_c
 
 void lb_set_grid_current(enum lb_phase phase, uint16_t current) {
   grid_current[phase] = current;
-  grid_current_age = 0;
+  fallback_time = config.fallback_limit_wait_time;
 }
 
 void lb_set_charger_limit_override(uint16_t limit) {
@@ -58,37 +58,24 @@ static void set_state(enum lb_state state_) {
   }
 }
 
-static void lb_check(uint32_t now) {
-  static uint32_t last_update = 0;
+static void lb_check(void) {
   uint16_t grid_current_max = get_max_grid_current();
-  uint32_t time_diff = 0;
 
-  if (last_update) {
-    time_diff = now - last_update;
-  }
-  last_update = now;
-
-  if (wait_time != WAIT_TIME_UNSET) {
-    if (wait_time > 0 && time_diff < wait_time) {
-      wait_time -= time_diff;
-    } else {
-      wait_time = 0;
-    }
+  if (wait_time > 0 && wait_time != WAIT_TIME_UNSET) {
+    wait_time--;
   }
 
-  if (grid_current_age < 0xFF && time_diff < (0xFF - grid_current_age)) {
-    grid_current_age += time_diff;
-  } else {
-    grid_current_age = 0xFF;
+  if (fallback_time > 0) {
+    fallback_time--;
   }
 
-  if (grid_current_age >= config.fallback_limit_wait_time) {
+  if (fallback_time == 0) {
     set_state(LB_STATE_FALLBACK);
   } else if (grid_current_max > config.alarm_limit) {
     set_state(LB_STATE_ALARM_LIMIT);
   } else if (grid_current_max > config.upper_limit) {
     set_state(LB_STATE_UPPER_LIMIT);
-  } else if (grid_current_max <= config.lower_limit) {
+  } else if (grid_current_max <= config.lower_limit && charger_max_current < config.charger_limit) {
     set_state(LB_STATE_LOWER_LIMIT);
   } else {
     set_state(LB_STATE_NORMAL);
@@ -166,8 +153,8 @@ void lb_task(uint32_t now) {
   static uint32_t timeout = 0;
 
   if (timeout == 0 || now - timeout > CHECK_INTERVAL_MS) {
-    lb_check(now / 1000);
     timeout = now;
+    lb_check();
   }
 }
 
